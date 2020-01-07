3aadd744512691564e8617e021de3c3e603ef684
Vikram <58028680+bcalm@users.noreply.github.com>
2020-01-07 23:12:34 +0530

    Added: normal photo and name of all users in home.html

--- resources/fetchData.js
+++ resources/fetchData.js
@@ -1,2 +1,15 @@
-const fs = require('fs');
-const githubData = require('./step7GithubData.json');
+const fs = require("fs");
+const githubData = require("./step7GithubData.json");
+
+for (let i = 0; i < githubData.length; i++) {
+  console.log(`<div class="image">`);
+  console.log(`<div class="imageSize">`);
+  console.log("<img");
+  console.log(`src="${githubData[i]["avatar_url"]}"`);
+  console.log(` class="imgArea"`);
+  console.log(`alt="${githubData[i].name}"`);
+  console.log("/>");
+  console.log("</div>");
+  console.log(`<div class="name">${githubData[i].name || githubData[i].login}</div>`);
+  console.log(" </div>");
+}
