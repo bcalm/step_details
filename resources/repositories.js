a59e4bdbb2c5b4e9f81feb7f9711759623f6cc28
Vikram <58028680+bcalm@users.noreply.github.com>
2020-01-08 01:45:25 +0530

    Added: batch6 data

--- resources/repositories.js
+++ resources/repositories.js
@@ -1,12 +1,86 @@
 const child = require("child_process").execSync;
 const fs = require("fs");
-const userName = "nooranasrin,rey-vthi,anil-muraleedharan,photongupta,Neha-sanserwal,satheesh-chandran,bugdriver,anujachaitanya,apurvagurme,drishya-dobriyal,lazyhackerthani,bernie-walker,sravani-labala,symbiote-ux,bsanthoshkumar,naveen-kumar-vadla,myultimatevision,imvaishu,desibabua,bcalm,shankarbyageli,melodyni,abhilashkasula,dad-ka-raneesa,shiviraj,cricket-lover,palpriyanshu,unphydra,venkybavisetti,rahitkar,mildshower,gulsane,pssruthy,trinangkur,armanaaquib,sukhiboi".split(
-  ","
-);
+
+const userName = [
+  "craftybones",
+  "vharidas",
+  "blpabhishek",
+  "reshmisaji",
+  "keerthyeb",
+  "swapnillothe",
+  "rahulmadaan",
+  "shubhiamchauhan",
+  "affishaikh",
+  "arnabghs",
+  "sapanaKale",
+  "leelanakka",
+  "mustakip",
+  "azimarif",
+  "bsrushti",
+  "kukshalkanishka",
+  "srijayanth",
+  "durgaprasadd",
+  "gayatrijagtap",
+  "cksharma11",
+  "dheeraj-p",
+  "brain-eater",
+  "athulpram",
+  "anupingale",
+  "luckyganesh",
+  "rishabghosh",
+  "luciferankon",
+  "heerajoshi",
+  "vprince001",
+  "deepikabartwal",
+  "swagatachakraborty",
+  "moumitaguri",
+  "tushartambe",
+  "sgauravk",
+  "akankshakutal",
+  "namankrs",
+  "maheshsovani",
+  "sumandeepak8",
+  "nooranasrin",
+  "rey-vthi",
+  "anil-muraleedharan",
+  "photongupta",
+  "Neha-sanserwal",
+  "satheesh-chandran",
+  "bugdriver",
+  "anujachaitanya",
+  "apurvagurme",
+  "drishya-dobriyal",
+  "lazyhackerthani",
+  "bernie-walker",
+  "sravani-labala",
+  "symbiote-ux",
+  "bsanthoshkumar",
+  "naveen-kumar-vadla",
+  "myultimatevision",
+  "imvaishu",
+  "desibabua",
+  "bcalm",
+  "shankarbyageli",
+  "melodyni",
+  "abhilashkasula",
+  "dad-ka-raneesa",
+  "shiviraj",
+  "cricket-lover",
+  "palpriyanshu",
+  "unphydra",
+  "venkybavisetti",
+  "rahitkar",
+  "mildshower",
+  "gulsane",
+  "pssruthy",
+  "trinangkur",
+  "armanaaquib",
+  "sukhiboi"
+];
 
 userName.forEach(link => {
   child(
-    `curl -u bcalm:e240ee7da5ee28f16585b008dde6f63ce8fdb721  https://api.github.com/users/${link} > ${link}.txt`
+    `curl -u bcalm:9278d5f03af17b4b78353af862c3a9796b10acf1  https://api.github.com/users/${link} > ${link}.txt`
   );
 });
 
