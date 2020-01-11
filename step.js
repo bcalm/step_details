51f72a7c7267b1faa4301c68d64ddf1484c33e1d
Vikram <58028680+bcalm@users.noreply.github.com>
2020-01-11 06:58:22 +0530

    Added: side bar with Member List

--- step.js
+++ step.js
@@ -1,8 +1,8 @@
-const mo = function (e) {
+const rotateBack = function (e) {
   e.children[0].style["display"] = "none";
   e.children[1].style["display"] = "block";
 };
-const mou = function (e) {
+const rotateFront = function (e) {
   e.children[0].style["display"] = "block";
   e.children[1].style["display"] = "none";
 };
@@ -12,12 +12,26 @@ const filterUser = (user, text) => {
   return userName.startsWith(text);
 }
 
-
 const searchBar = function (searchString) {
   const text = document.getElementById('searchBox').value.toLowerCase();
-  const allUsers = Array.from(document.getElementsByClassName('user'))
-  console.log(allUsers)
+  const allUsers = Array.from(document.getElementsByClassName('user'));
   allUsers.forEach(user => user.style.display = 'none')
   const filteredUser = allUsers.filter(user => filterUser(user, text))
   filteredUser.forEach(user => user.style['display'] = 'flex')
 }
+
+const openSideBar = function (e) {
+  const sideBar = document.getElementsByClassName('sideBar')[0];
+  sideBar.style["display"] = 'block';
+}
+
+const closeSideBar = function (e) {
+  document.getElementsByClassName('sideBar')[0].style.display = "none";
+}
+
+const createSideBar = function () {
+  const list = Array.from(document.getElementsByClassName('userList'));
+  const users = Array.from(document.getElementsByClassName('name'));
+  const userList = users.map(user => "✪  " + user.innerText)
+  list[0].innerText += "\n" + userList.join('\n');
+};
